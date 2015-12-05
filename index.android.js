0e4ecf9e2821c62148ea79ce5a53d1e277ea396d
nonmadden <nonmadden@gmail.com>
2015-12-05 20:26:16 +0700

    Testing

--- index.android.js
+++ index.android.js
@@ -1,52 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+
 'use strict';
 
 var React = require('react-native');
+var Featured = require('./app/Featured');
+var Search = require('./app/Search')
+
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+    AppRegistry,
+    TabBarIOS,
+    Component
+   } = React;
 
-var OhmpiengApp = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+class OhmpiengApp extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          selectedTab: 'featured'
+      };
   }
-});
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  render() {
+    render: function() {
+      return (
+        <ToolbarAndroid
+          logo={require('image!app_logo')}
+          title="AwesomeApp"
+          actions={[{title: 'Settings', icon: require('image!icon_settings'), show: 'always'}]}
+          onActionSelected={this.onActionSelected} />
+      )
+    },
+    onActionSelected: function(position) {
+      if (position === 0) { // index of 'Settings'
+        showSettings();
+      }
+    }
+  }
+}
 
 AppRegistry.registerComponent('OhmpiengApp', () => OhmpiengApp);
