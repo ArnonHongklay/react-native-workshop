64999c608d7173173340c13c97332bf7a3874e61
nonmadden <nonmadden@gmail.com>
2015-12-06 07:58:20 +0700

    Test android

--- index.android.js
+++ index.android.js
@@ -1,41 +1,21 @@
-
 'use strict';
 
 var React = require('react-native');
-var Featured = require('./app/Featured');
-var Search = require('./app/Search')
+
 
 var {
     AppRegistry,
-    TabBarIOS,
-    Component
-   } = React;
+    Component,
+    Navigator,
+    MySceneComponent
+  } = React;
 
-class OhmpiengApp extends Component {
+var styles = React.StyleSheet.create({
 
-  constructor(props) {
-      super(props);
-      this.state = {
-          selectedTab: 'featured'
-      };
-  }
+});
 
-  render() {
-    render: function() {
-      return (
-        <ToolbarAndroid
-          logo={require('image!app_logo')}
-          title="AwesomeApp"
-          actions={[{title: 'Settings', icon: require('image!icon_settings'), show: 'always'}]}
-          onActionSelected={this.onActionSelected} />
-      )
-    },
-    onActionSelected: function(position) {
-      if (position === 0) { // index of 'Settings'
-        showSettings();
-      }
-    }
-  }
-}
+var OhmpiengApp = React.createClass({
 
-AppRegistry.registerComponent('OhmpiengApp', () => OhmpiengApp);
+});
+
+React.AppRegistry.registerComponent('OhmpiengApp', () => OhmpiengApp);
