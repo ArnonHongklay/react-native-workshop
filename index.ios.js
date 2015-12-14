d2da2044aaa11ebc4e66e1aca2875fcb2b03f086
nonmadden <nonmadden@gmail.com>
2015-12-14 17:31:04 +0700

    move inside

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,118 @@
-
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
 'use strict';
 
-var React     = require('react-native');
-var Featured  = require('./app/Featured');
-var Search    = require('./app/Search')
-
+var React = require('react-native');
 var {
-    AppRegistry,
-    TabBarIOS,
-    Component
-  } = React;
+  AppRegistry,
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+} = React;
 
-class OhmpiengApp extends Component {
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTab: 'featured'
+var OhmpiengApp = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
     };
-  }
+  },
 
-  render() {
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
     return (
-      <TabBarIOS selectedTab={this.state.selectedTab}>
-        <TabBarIOS.Item
-          selected={this.state.selectedTab === 'featured'}
-          systemIcon='bookmarks'
-          icon=""
-          onPress={() => {
-            this.setState({
-              selectedTab: 'featured'
-            });
-          }}>
-          <Featured/>
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          selected={this.state.selectedTab === 'search'}
-          systemIcon='search'
-          icon=""
-          onPress={() => {
-            this.setState({
-              selectedTab: 'search'
-            });
-          }}>
-          <Search/>
-        </TabBarIOS.Item>
-      </TabBarIOS>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
     );
-  }
-}
+  },
+
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  },
+
+  renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
 
 AppRegistry.registerComponent('OhmpiengApp', () => OhmpiengApp);
