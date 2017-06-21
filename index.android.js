d9c352176fa1eea8e561b0002292e01d0d0cc919
Arnon Hongklay <arnon@hongklay.com>
2017-06-22 06:52:16 +0700

    react native workshop

--- index.android.js
+++ index.android.js
@@ -1,14 +1,53 @@
-var React = require('react-native');
-var { requireNativeComponent } = React;
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-class OhmpiengApp extends React.Component {
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class ReactNativeWorkShop extends Component {
   render() {
-    return <NativeOhmpiengApp {...this.props} />;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
   }
 }
-OhmpiengApp.propTypes = {
-  myCustomProperty: React.PropTypes.oneOf(['a', 'b']),
-};
 
-var NativeOhmpiengApp = requireNativeComponent('OhmpiengApp', OhmpiengApp);
-module.exports = OhmpiengApp;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ReactNativeWorkShop', () => ReactNativeWorkShop);
--- index.ios.js
+++ index.ios.js
@@ -1,35 +1,53 @@
-// iOS
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-// var React = require('react-native');
-// var { TabBarIOS, NavigatorIOS } = React;
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-// var App = React.createClass({
-//   render: function() {
-//     return (
-//       <TabBarIOS>
-//         <TabBarIOS.Item title="React Native" selected={true}>
-//           <NavigatorIOS initialRoute={{ title: 'React Native' }} />
-//         </TabBarIOS.Item>
-//       </TabBarIOS>
-//     );
-//   },
-// });
-
-var React = require('react-native');
-var { NativeModules, Text } = React;
-
-var Message = React.createClass({
-  getInitialState() {
-    return { text: 'Goodbye World.' };
-  },
-  componentDidMount() {
-    NativeModules.MyCustomModule.processString(this.state.text, (text) => {
-      this.setState({text});
-    });
-  },
-  render: function() {
+export default class ReactNativeWorkShop extends Component {
+  render() {
     return (
-      <Text>{this.state.text}</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
     );
   }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
+
+AppRegistry.registerComponent('ReactNativeWorkShop', () => ReactNativeWorkShop);
